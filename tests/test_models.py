75e33eb6a40079842c42ae7d5f24bc353edbdf39
4shen0ne <4shen.01@gmail.com>
2024-08-14 21:52:14 +0800

    ✅ add a simple test

---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import pytest
+
+from nmass.models import NmapRun
+
+
+@pytest.fixture
+def xml_files() -> list[Path]:
+    return list(Path("tests/data").glob("**/*.xml"))
+
+
+def test_parse_xml_without_errors(xml_files):
+    for filename in xml_files:
+        try:
+            with open(filename) as file:
+                nr = NmapRun.from_xml(file.read())
+                assert nr.scanner in ("nmap", "masscan")
+                print(f"✔ {filename}")
+        except Exception:
+            pytest.fail(f"Parsing {filename} failed 😢")
